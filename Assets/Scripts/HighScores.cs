06d6a22fda555baa4d76972dde5c4df5bde24efe
Sarah Herzog <235290+CodingDino@users.noreply.github.com>
2018-12-14 15:22:30 +0000

    High Score system complete

--- Assets/Scripts/HighScores.cs
+++ Assets/Scripts/HighScores.cs
@@ -19,6 +19,20 @@ public class HighScores : MonoBehaviour {
         // Load the high score data from the PlayerPrefs
         LoadHighScoreData();
 
+        // Get our current score from PlayerPrefs
+        int currentScore = PlayerPrefs.GetInt("score", 0);
+
+        // Check if we got a new high score
+        bool haveNewHighScore = IsNewHighScore(currentScore);
+        if (haveNewHighScore == true)
+        {
+            // Add new score to the data
+            AddScoreToList(currentScore);
+
+            // Save updated data
+            SaveHighScoreData();
+        }
+        
         // Update the visual display
         UpdateVisualDisplay();
 	}
@@ -52,4 +66,69 @@ public class HighScores : MonoBehaviour {
             highScoreDisplays[i].text = highScoreData[i].ToString();
         }
     }
+
+    private bool IsNewHighScore(int scoreToCheck)
+    {
+        // Loop through the high scores and see if ours is higher than any of them
+        for (int i = 0; i < highScoreDisplays.Count; ++i)
+        {
+            // Is our score higher than the high score we're checking this loop?
+            if (scoreToCheck > highScoreData[i])
+            {
+                // Our score is higher!
+                // Return to the calling code that we DO have a new high score
+                return true;
+            }
+        }
+
+        // default: false
+        // We did NOT find any scores that our score was higher than...
+        return false;
+    }
+
+    private void AddScoreToList(int newScore)
+    {
+        // Loop through the high scores and find where the new score fits
+        for (int i = 0; i < highScoreDisplays.Count; ++i)
+        {
+            // Is our score higher than the score we're checking in the list?
+            if (newScore > highScoreData[i])
+            {
+                // Our score IS higher
+                // Since we're going from highest to lowest, the first
+                // time our score is higher, this is where it must go
+
+                // Insert the new score into the list here
+                highScoreData.Insert(i, newScore);
+
+                // Trim the last item off the list
+                highScoreData.RemoveAt(highScoreData.Count - 1);
+
+                // We're done, we must exit early
+                return; 
+            }
+
+        }
+    }
+
+
+    private void SaveHighScoreData()
+    {
+        for (int i = 0; i < highScoreDisplays.Count; ++i)
+        {
+            // Using the loop index, get the name for our PlayerPrefs key
+            string prefsKey = "highScore" + i.ToString();
+
+            // Get the current high score entry from the data
+            int highScoreEntry = highScoreData[i];
+
+            // Save this data to the PlayerPrefs
+            PlayerPrefs.SetInt(prefsKey, highScoreEntry);
+        }
+
+        // Save the player prefs to disk
+        PlayerPrefs.Save();
+    }
+
+
 }
